70288bf6f08b1368a98f322a1400a6c73139bec9
nikitaamani <nikita.bosire@student.moringaschool.com>
2025-01-28 10:35:23 +0300

    add proxy configuration for API requests

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,30 +1,15 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { fileURLToPath } from "url";
-// Get the equivalent of __dirname in ESM
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@components": path.resolve(__dirname, "./src/components"),
-      "@assets": path.resolve(__dirname, "./src/assets"),
-      "@pages": path.resolve(__dirname, "./src/pages"),
-      "@utils": path.resolve(__dirname, "./src/utils"),
-    },
-  },
   server: {
-    port: 4000,
-    open: true,
     proxy: {
-      "/api": {
-        target: "http://localhost:5555",
-        changeOrigin: true,
-        secure: false,
+      '/api': {
+        target: 'http://localhost:5555',
+        changeOrigin: true, // Change the origin of the request to the target URL
+        secure: false,      // Allow proxying to servers with self-signed SSL certificates
+        rewrite: (path) => path.replace(/^\/api/, ''), // Remove the `/api` prefix
       },
     },
   },
